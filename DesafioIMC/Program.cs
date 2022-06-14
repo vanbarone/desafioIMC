3cec7cc6dd1adcb99d9bec6a1a7d76eb7c2be95e
Vanessa Barone <vanbarone.milani@gmail.com>
2022-06-14 12:19:32 -0300

    Declaração das variaveis e leitura do nome e sexo

--- DesafioIMC/Program.cs
+++ DesafioIMC/Program.cs
@@ -6,7 +6,29 @@ namespace DesafioIMC
     {
         static void Main(string[] args)
         {
-            
+            //declaração das variáveis
+            string nome = "";
+            string sexo = "";
+            int idade = 0;
+            double altura = 0;
+            double peso = 0;
+
+            Console.WriteLine("Olá");
+            Console.WriteLine("Faremos agora um diagnóstico prévio da sua saúde\n");
+            Console.WriteLine("Para isso preciso de algumas informações =>");
+
+            //leitura das informações
+            Console.Write("Por favor informe seu nome: ");
+            nome = Console.ReadLine();
+
+            do
+            {
+                Console.Write("Por favor informe seu sexo (digite 'F' para Feminino e 'M' para Masculino): ");
+                sexo = Console.ReadLine().ToUpper();
+            } while (sexo != "F" && sexo != "M");
+
+
+
 
 
 
